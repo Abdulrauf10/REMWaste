353835a9bced12eec556ec6237a1f171629b3226
Abdulrauf10 <abd.rauf.lamada@gmail.com>
2025-03-25 22:17:55 +0700

    add env

--- src/hook/api/useFetchApi.ts
+++ src/hook/api/useFetchApi.ts
@@ -2,12 +2,11 @@
 import { useQuery } from "@tanstack/react-query"
 
 export function useFetchApi() {
+  const baseApi = import.meta.env.VITE_API_URL
   return useQuery({
     queryKey: ["data"],
     queryFn: async () => {
-      const res = await fetch(
-        "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
-      )
+      const res = await fetch(baseApi)
       if (!res.ok) throw new Error("Network response was not ok")
       return res.json()
     }
